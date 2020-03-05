5d596cf340ab65e0ca041dde4fed0d4e0186b3ee
SuzannAdams <suzannyee@yahoo.com>
2020-03-05 09:47:47 -0800

    initial set up and model

---
+++ boba/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Meet(models.Model):
+    name = models.CharField(max_length=100)
+    place = models.CharField(max_length=100)
+    time = models.CharField(max_length=100)
+    topic = models.CharField(max_length=100)
+    photo_url = models.TextField()
+    def __str__(self):
+        return self.name
