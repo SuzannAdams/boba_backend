a684287fb6cd2a3aff536aee37fb96c57f51ea94
SuzannAdams <suzannyee@yahoo.com>
2020-03-07 11:52:01 -0800

    added secret key to heroku

--- boba_django/settings.py
+++ boba_django/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['sj0s8^nb&-quq8-pj_xy!ctoh^g@r7&vlv&4+=90zalbr55xli']
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
